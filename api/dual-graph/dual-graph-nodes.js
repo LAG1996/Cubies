3ff6d55fdbd71438e702d887c720f6342946a6be
LAG1996 <luis.a.garcia01996@gmail.com>
2018-06-25 16:36:39 -0500

    Introduced console logs for when dual graph components have too many neighbors, fixed bug where some edges in edge neighborhoods are not displayed

--- api/dual-graph/dual-graph-nodes.js
+++ api/dual-graph/dual-graph-nodes.js
@@ -1,17 +1,18 @@
 //Classes representing nodes of the face dual graph
 //Private static members for the FaceNode class
-const FN_PRIVATES = new WeakMap();
+const NODE_PRIVATES = new WeakMap();
 
 export class FaceNode{
 	constructor(faceID, facePosition, faceNormal, parentCubePosition){
 		//Defining private variables
-		FN_PRIVATES.set(this, {
+		NODE_PRIVATES.set(this, {
 			id: faceID,
 			neighbors: [],
 			edges: [],
 			normal: faceNormal,
 			position: facePosition,
-			parentCubePosition: parentCubePosition
+			parentCubePosition: parentCubePosition,
+			hadTooManyNeighbors: false
 		});
 
 		this.visited = false;
@@ -19,84 +20,81 @@ export class FaceNode{
 
 	//getters
 	get ID(){
-		return FN_PRIVATES.get(this).id;
+		return NODE_PRIVATES.get(this).id;
 	}
 
 	get neighbors(){
-		return [...FN_PRIVATES.get(this).neighbors];
+		return [...NODE_PRIVATES.get(this).neighbors];
 	}
 
 	get position(){
-		return FN_PRIVATES.get(this).position.clone();
+		return NODE_PRIVATES.get(this).position.clone();
 	}
 
 	get normal(){
-		return FN_PRIVATES.get(this).normal.clone();
+		return NODE_PRIVATES.get(this).normal.clone();
 	}
 
 	get parentCubePosition(){
-		return FN_PRIVATES.get(this).parentCubePosition.clone();
+		return NODE_PRIVATES.get(this).parentCubePosition.clone();
 	}
 
 	get edges(){
-		return [...FN_PRIVATES.get(this).edges];
+		return [...NODE_PRIVATES.get(this).edges];
 	}
 
 	//Add neighbors to the neighbor list. Release a warning if this face
 	//has more than 4 neighbors.
 	addNeighbor(faceNode){
-		let neighbors = FN_PRIVATES.get(this).neighbors;
+		let neighbors = NODE_PRIVATES.get(this).neighbors;
 
 		if(neighbors.includes(faceNode)) return;
 
-		try{
-			if(neighbors.length >=4 ){
-				throw "Face #" + this.ID + " has more than 4 neighbors. The polycube face dual graph is not 4-regular.";
-			}
-		}
-		catch(err){
-			console.error(err);
-		}
-		finally{
-			neighbors.push(faceNode);
+		neighbors.push(faceNode);
+
+		if(hasTooManyNeighbors(this, 4)){
+			console.log("Face #" + this.ID + " has more than 4 neighbors. The polycube face dual graph is not 4-regular.");
+			NODE_PRIVATES.get(this).hadTooManyNeighbors = true;
 		}
 	}
 
 	addEdge(edgeNode){
-		FN_PRIVATES.get(this).edges.push(edgeNode);
+		NODE_PRIVATES.get(this).edges.push(edgeNode);
 	}
 
 	//Remove the specified neighbor
 	removeNeighbor(faceNode){
-		let neighbors = FN_PRIVATES.get(this).neighbors;
+		let neighbors = NODE_PRIVATES.get(this).neighbors;
 
 		neighbors.splice(neighbors.indexOf(faceNode), 1);
+
+		if(hadTooManyNeighbors(this, 4)){
+			console.log("Edge #" + this.ID + " now has a safe amount of neighbors. Neighbor count: " + (this.neighbors.length));
+			NODE_PRIVATES.get(this).hadTooManyNeighbors = false;
+		}
 	}
 
 	destroy(){
 
 		//Destroy all edges attached to this face
-		FN_PRIVATES.get(this).edges.map((edge) => {
+		NODE_PRIVATES.get(this).edges.map((edge) => {
 			edge.destroy();
 		});
 
 		//Tell all neighbors to forget about this node
-		FN_PRIVATES.get(this).neighbors.map((neighbor) => {
+		NODE_PRIVATES.get(this).neighbors.map((neighbor) => {
 			neighbor.removeNeighbor(this);
 		});
 
 		//Remove references to this face's private members
-		FN_PRIVATES.delete(this);
+		NODE_PRIVATES.delete(this);
 	}
 
 }
 
-//Private static members for the EdgeNode class
-const EN_PRIVATES = new WeakMap();
-
 export class EdgeNode{
 	constructor(edgeID, edgePosition, edgeEndpoints, edgeAxis, parentFace){
-		EN_PRIVATES.set(this, {
+		NODE_PRIVATES.set(this, {
 			id: edgeID,
 			neighbors: [],
 			endpoints: edgeEndpoints,
@@ -104,37 +102,38 @@ export class EdgeNode{
 			position: edgePosition,
 			isBoundary: true,
 			incidentEdge: null,
-			parent: parentFace
+			parent: parentFace,
+			hadTooManyNeighbors: false
 		})
 	}
 
 	//getters
 	get ID(){
-		return EN_PRIVATES.get(this).id;
+		return NODE_PRIVATES.get(this).id;
 	}
 
 	get position(){
-		return EN_PRIVATES.get(this).position.clone();
+		return NODE_PRIVATES.get(this).position.clone();
 	}
 
 	get neighbors(){
-		return [...EN_PRIVATES.get(this).neighbors];
+		return [...NODE_PRIVATES.get(this).neighbors];
 	}
 
 	get endpoints(){
-		return [...EN_PRIVATES.get(this).endpoints];
+		return [...NODE_PRIVATES.get(this).endpoints];
 	}
 
 	get axis(){
-		return EN_PRIVATES.get(this).axis.clone();
+		return NODE_PRIVATES.get(this).axis.clone();
 	}
 	
 	get incidentEdge(){
-		return EN_PRIVATES.get(this).incidentEdge;
+		return NODE_PRIVATES.get(this).incidentEdge;
 	}
 
 	get parent(){
-		return EN_PRIVATES.get(this).parent;
+		return NODE_PRIVATES.get(this).parent;
 	}
 
 	get isBoundary(){
@@ -144,17 +143,17 @@ export class EdgeNode{
 	//setters
 	set incidentEdge(edgeNode){
 		//console.log("Edge #" + this.ID + ": now incident with edge#" + edgeNode.ID); 
-		EN_PRIVATES.get(this).incidentEdge = edgeNode;
+		NODE_PRIVATES.get(this).incidentEdge = edgeNode;
 	}
 
 	getAllNeighbors(){
 		let neighborNodes = [];
 
-		EN_PRIVATES.get(this).neighbors.map((neighbor) => {
+		NODE_PRIVATES.get(this).neighbors.map((neighbor) => {
 			neighborNodes.push(neighbor);
 
 			if(!neighbor.isBoundary){
-				neighborNodes.push(neighbor);
+				neighborNodes.push(neighbor.incidentEdge);
 			}
 		});
 
@@ -171,30 +170,30 @@ export class EdgeNode{
 	}
 
 	addNeighbor(edgeNode){
-		let neighbors = EN_PRIVATES.get(this).neighbors;
+		let neighbors = NODE_PRIVATES.get(this).neighbors;
+		
 		if(neighbors.includes(edgeNode)) return;
+		
+		neighbors.push(edgeNode);
 
-		try{
-			if(neighbors.length >= 8)
-				throw "Edge #" + this.ID + " has " + (this.neighbors.length + 1) + " neighbors. Edge graph is no longer of maximum degree 8."
-		}
-		catch(err){
-			console.error(err);
-		}
-		finally{
-			//console.log("Edge #" + this.ID + ": setting edge #" + edgeNode.ID + " as a neighbor");
-			neighbors.push(edgeNode);
+		if(hasTooManyNeighbors(this, 8)){
+			console.log("Edge #" + this.ID + " has " + (this.neighbors.length) + " neighbors. Edge graph is no longer of maximum degree 8.");
+			NODE_PRIVATES.get(this).hadTooManyNeighbors = true;
 		}
 	}
 
 	removeNeighbor(edgeNode){
-		let neighbors = EN_PRIVATES.get(this).neighbors;
+		let neighbors = NODE_PRIVATES.get(this).neighbors;
 
 		neighbors.splice(neighbors.indexOf(edgeNode), 1);
+
+		if(hadTooManyNeighbors(this, 8)){
+			console.log("Edge #" + this.ID + " now has a safe amount of neighbors. Neighbor count: " + (this.neighbors.length));
+		}
 	}
 
 	removeIncidentEdge(){
-		EN_PRIVATES.get(this).incidentEdge = null;
+		NODE_PRIVATES.get(this).incidentEdge = null;
 	}
 
 	//Delete all internal references to this node.
@@ -202,15 +201,23 @@ export class EdgeNode{
 		//tell all neighbors to forget this node
 		//console.log("Deleting edge#" + this.ID);
 
-		EN_PRIVATES.get(this).neighbors.map((neighborEdge) => {
+		NODE_PRIVATES.get(this).neighbors.map((neighborEdge) => {
 			neighborEdge.removeNeighbor(this);
 		})
 
 		//tell the incident edge to forget this node
 		if(!this.isBoundary){
-			EN_PRIVATES.get(this).incidentEdge.removeIncidentEdge();
+			NODE_PRIVATES.get(this).incidentEdge.removeIncidentEdge();
 		}
 
-		EN_PRIVATES.delete(this);
+		NODE_PRIVATES.delete(this);
 	}
+}
+
+function hasTooManyNeighbors(node, maximum){
+	return node.neighbors.length > maximum;
+}
+
+function hadTooManyNeighbors(node, maximum){
+	return NODE_PRIVATES.get(node).hadTooManyNeighbors && !hasTooManyNeighbors(node, maximum);
 }
